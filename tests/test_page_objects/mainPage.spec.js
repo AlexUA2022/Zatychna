287ec5c601a91cd52db99c935aaed2a1862949df
Alexander Saponov <98758052+AlexUA2022@users.noreply.github.com>
2024-06-02 08:47:09 +0300

    RF_POM | mainPage.spec | ТС.01.01.15 Verify that the "Новинки" section contains pagination, ТС.01.01.16 Verify that the pagination contains "Next" (>) button, ТС.01.01.17 Verify that the "Next" (>) button has a pointer cursor, ТС.01.01.18 Verify that the images of products move forward after clicking on the  "Next" (>) button, ТС.01.01.19 Verify that the pagination contains "Prev" (<) button, ТС.01.01.20 Verify that the "Prev" (<) button has a auto cursor, ТС.01.01.20.1 Verify that the images of products move back after clicking on the  "Prev" (<) button (#3)

--- tests/test_page_objects/mainPage.spec.js
+++ tests/test_page_objects/mainPage.spec.js
@@ -121,6 +121,76 @@ test.describe('mainPage.spec', () => {
 
 	});
 
+	test('ТС.01.01.15 Verify that the "Новинки" section contains pagination', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		await expect(homePage.locators.getNoveltiesSectionPagination()).toBeVisible();
+
+	});
+
+	test('ТС.01.01.16 Verify that the pagination contains "Next" (>) button', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		await expect(homePage.locators.getNextBtn()).toBeVisible();
+
+	});
+
+	test('ТС.01.01.17 Verify that the "Next" (>) button has a pointer cursor', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		await expect(homePage.locators.getNextBtn()).toBeVisible();
+		await expect(homePage.locators.getNextBtn()).toHaveCSS('cursor', 'pointer');
+
+	});
+
+	test('ТС.01.01.18 Verify that the images of products move forward after clicking on the  "Next" (>) button', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		await homePage.clickNextBtn();
+		await homePage.clickNextBtn();
+		await homePage.clickNextBtn();
+		await homePage.clickNextBtn();
+
+		await expect(homePage.locators.getNoveltiesSection()).toBeVisible();
+
+	});
+
+	test('ТС.01.01.19 Verify that the pagination contains "Prev" (<) button', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		await expect(homePage.locators.getPrevBtn()).toBeVisible();
+
+	});
+
+	test('ТС.01.01.20 Verify that the "Prev" (<) button has a auto cursor', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		await expect(homePage.locators.getPrevBtn()).toBeVisible();
+		await expect(homePage.locators.getPrevBtn()).toHaveCSS('cursor', 'auto');
+
+	});
+
+	test('ТС.01.01.20.1 Verify that the images of products move back after clicking on the  "Prev" (<) button', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		await homePage.clickNextBtn();
+		await homePage.clickNextBtn();
+		await homePage.clickNextBtn();
+		await homePage.clickNextBtn();
+
+		await expect(homePage.locators.getNoveltiesSection()).toBeVisible();
+
+		await homePage.clickPrevBtn();
+		await homePage.clickPrevBtn();
+		await homePage.clickPrevBtn();
+		await homePage.clickPrevBtn();
+
+		await expect(homePage.locators.getNoveltiesSection()).toBeVisible();
+
+	});
+
+
+
 	
 })
 
