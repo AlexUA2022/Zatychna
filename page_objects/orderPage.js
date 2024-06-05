9dc09bef00777d1bec2a78b7a25664c54b999f20
Katerina Poliit <alexispoliyit@gmail.com>
2024-06-05 13:08:23 +0300

    RF_POM | orderPage.spec | TC 04.01.8 Verify that the page contains an information section about the product, TC 04.01.10 Verify that the section "Особисті дані, TC 04.01.10.1 Verify that the "Особисті дані" section contains the mandatory "Прізвище *"field, TC 04.01.10.1 Verify that the"Прізвище" field contains only letters, a message was received when entering invalid data', TC 04.01.11.1 Verify that the "Особисті дані" section contains the mandatory "Ім’я *"field', TC 04.01.12 Verify that the"Ім’я" field contains only letters, a message was received when entering invalid data

--- page_objects/orderPage.js
+++ page_objects/orderPage.js
@@ -13,7 +13,27 @@ class OrderPage {
 		getProductSize: ()  => this.page.getByText('S', { exact: true }),
 		getQuantityProduct: ()  => this.page.getByText('x'),
 		getCostProduct: () => this.page.getByText('990 UAH').first(),
+		getInformationSctionProduct: () => this.page.locator('section').filter({ hasText: '1 товари(-ів) на суму3 990' }),
+		getPersonalDataSection: () => this.page.getByText('1Особисті дані'),
+		getNameField: () => this.page.getByPlaceholder('Прізвище'),
+		getOrderButton: () => this.page.getByRole('button', { name: 'Оформити замовлення' }),
+		getMessageWarnings: () => this.page.getByText('Поле приймає тільки кирилицю та латиницю'),
+		getName2Field: () => this.page.getByPlaceholder('Ім’я'),
+
     }
+
+	async clickOrderButton() {
+		await this.locators.getOrderButton().click();
+	}
+
+	async filldigitNameField() {
+		await this.locators.getNameField().fill('666');
+	}
+
+	async filldigitName2Field() {
+		await this.locators.getName2Field().fill('3535');
+	}
+
 
 
 
